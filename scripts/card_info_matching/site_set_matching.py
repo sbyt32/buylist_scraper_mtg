472a738882ffa1a917401b141d0942634d33aa39
Riz <whoisrizvi@gmail.com>
2022-10-28 16:08:11 -0700

    site set matching: Iterate over file directly

--- scripts/card_info_matching/site_set_matching.py
+++ scripts/card_info_matching/site_set_matching.py
@@ -11,15 +11,13 @@ def main():
         file_data[site_name] = {}
 
         with open(f'data/{site_name}/set_name.txt', 'r') as set_name_file:
-            set_names = set_name_file.readlines()
+            for set_name in set_name_file:
+                set_name = set_name.strip()
 
-        for set_name in set_names:
-            set_name = set_name.rstrip('\n')
-
-            file_data[site_name][set_name] = {
-                "normal": set_name,
-                "tokens": set_name
-            }
+                file_data[site_name][set_name] = {
+                    "normal": set_name,
+                    "tokens": set_name
+                }
 
     with open(sets_info_location, 'w') as file:
         file.write(json.dumps(file_data, indent=4))
